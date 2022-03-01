0f2def99bae4155d8d228554d3fd2a7c59ea96d6
Charles Liu <charles.liu0309@gmail.com>
2022-03-01 06:57:05 -0500

    reconfigured ui library button

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,9 +1,16 @@
-import React, { FC } from 'react';
+import React from 'react';
 
-export const Button: FC = ({ children }) => {
+export type Props = {
+  children: React.ReactNode;
+} & React.ComponentPropsWithoutRef<'button'>;
+
+export const Button = React.forwardRef<HTMLButtonElement, Props>(function Button(
+  { children, type = 'button', ...props },
+  ref
+) {
   return (
-    <button type="button" className="bg-blue-500">
+    <button ref={ref} type={type} className="bg-blue-500" {...props}>
       {children}
     </button>
   );
-};
+});
